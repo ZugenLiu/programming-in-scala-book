03e0db4ac1ccafb06a8e893be448d8af74a9fec6
ranjankumar-gh <mail4ranjan@gmail.com>
2016-06-25 10:03:12 +0530

    First commit

---
+++ GreetStrings.scala
@@ -0,0 +1,9 @@
+val greetStrings = new Array[String](3)
+//val greetStrings: Array[String] = new Array[String](3)
+
+greetStrings.update(0, "Hello")
+greetStrings.update(1, ", ")
+greetStrings.update(2, "world!\n")
+
+for(i <- 0 to 2)
+  print(greetStrings.apply(i))
